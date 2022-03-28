78c216eb2197fd4f84abfe01acb0c38cb3ac50bd
yotsufe <yotsufe@gmail.com>
2022-03-28 23:15:44 +0900

    faltlane テスト追加

--- app/src/test/java/com/yotsufe/androidcicd/ExampleUnitTest.kt
+++ app/src/test/java/com/yotsufe/androidcicd/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.yotsufe.androidcicd
 
+import android.util.Log
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,10 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun string_isCorrect() {
+        assertEquals("4", (2 + 2).toString())
     }
 }
